85177654e02a2961d01f13874ca45ba9ecbf11f1
alvandakbar <alfiana72@gmail.com>
2019-02-06 00:36:05 +0700

    sasa

--- app/__init__.py
+++ app/__init__.py
@@ -16,9 +16,9 @@ from app import views
 def db_connect():
     g.con = mysql.connect(
         host = "localhost",
-        user = "root",
-        passwd = "ALFIAN123",
-        db = "db_fcm",
+        user = "upzpana1_ta",
+        passwd = "rosyidganteng123",
+        db = "upzpana1_ta",
         charset = 'utf8',
         use_unicode = True
     )
